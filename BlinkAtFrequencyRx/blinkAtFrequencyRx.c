e6e524e3f652fa5df828b938ee10ced3b80f1e4c
Mateus Henrique <mateush.study@gmail.com>
2022-05-24 21:18:01 -0300

    Initial commit

---
+++ BlinkAtFrequencyRx/blinkAtFrequencyRx.c
@@ -0,0 +1,116 @@
+/*  ======== blinkAtFrequencyRx.c ========
+*/
+
+/* XDC Module Headers */
+#include <xdc/runtime/System.h>
+
+/* For usleep() */
+#include <unistd.h>
+#include <stdint.h>
+#include <stddef.h>
+
+/* Driver Header files */
+#include <ti/drivers/GPIO.h>
+#include <ti/drivers/apps/LED.h>
+
+/* Driver configuration */
+#include "ti_drivers_config.h"
+
+/* EasyLink API Header files */
+#include "easylink/EasyLink.h"
+
+/* DIO6, LaunchPad LED Red */
+#define CONFIG_LED_0                    0
+
+/* DIO6, LaunchPad LED Green */
+#define CONFIG_LED_1                    1
+
+/* Number of LEDs */
+#define CONFIG_LEDCOUNT        2
+
+#define ONE_HUNDRED 100
+#define COMMAND_INDEX 0
+#define TIME_INDEX 1
+
+/* LED handle */
+LED_Handle ledHandle[CONFIG_LEDCOUNT];
+
+/* Packet that receives the data */
+EasyLink_RxPacket rxPacket = {{0}, 0, 0, 0, 0, {0}};
+
+/*
+ *  ======== mainThread ========
+ */
+void *mainThread(void *arg0)
+{
+    LED_Params ledParams;
+
+    LED_init();
+    Board_initGeneral();
+
+    /* Open LED0 and LED1 with default params */
+    LED_Params_init(&ledParams);
+    ledHandle[CONFIG_LED_0] = LED_open(CONFIG_LED_0, &ledParams);
+    ledHandle[CONFIG_LED_1] = LED_open(CONFIG_LED_1, &ledParams);
+
+    /* Initialize the EasyLink parameters to their default values */
+    EasyLink_Params easyLink_params;
+    EasyLink_Params_init(&easyLink_params);
+
+    /*
+     * Initialize EasyLink with the settings found in ti_easylink_config.h
+     * Modify EASYLINK_PARAM_CONFIG in ti_easylink_config.h to change the default
+     * PHY
+     */
+    if (EasyLink_init(&easyLink_params) != EasyLink_Status_Success)
+    {
+        System_abort("EasyLink_init failed");
+    }
+
+    while (1)
+    {
+        /* Receiving the packet */
+        EasyLink_Status result = EasyLink_receive(&rxPacket);
+
+        if (result == EasyLink_Status_Success)
+        {
+            /* Change LED according to the value received */
+            switch (rxPacket.payload[COMMAND_INDEX])
+            {
+                case 0:
+//                PIN_setOutputValue(pinHandle, CONFIG_PIN_RLED, 0);
+//                PIN_setOutputValue(pinHandle, CONFIG_PIN_GLED, 1);
+                    LED_startBlinking(ledHandle[CONFIG_LED_0],
+                                      rxPacket.payload[TIME_INDEX] * ONE_HUNDRED,
+                                      LED_BLINK_FOREVER);
+                    LED_stopBlinking(ledHandle[CONFIG_LED_1]);
+                    LED_setOff(ledHandle[CONFIG_LED_1]);
+                    break;
+
+                case 1:
+//                PIN_setOutputValue(pinHandle, CONFIG_PIN_RLED, 1);
+//                PIN_setOutputValue(pinHandle, CONFIG_PIN_GLED, 0);
+                    LED_startBlinking(ledHandle[CONFIG_LED_1],
+                                      rxPacket.payload[TIME_INDEX] * ONE_HUNDRED,
+                                      LED_BLINK_FOREVER);
+                    LED_stopBlinking(ledHandle[CONFIG_LED_0]);
+                    LED_setOff(ledHandle[CONFIG_LED_0]);
+                    break;
+
+                case 2:
+                    LED_startBlinking(ledHandle[CONFIG_LED_0],
+                                      rxPacket.payload[TIME_INDEX] * ONE_HUNDRED,
+                                      LED_BLINK_FOREVER);
+                    LED_startBlinking(ledHandle[CONFIG_LED_1],
+                                      rxPacket.payload[TIME_INDEX] * ONE_HUNDRED,
+                                      LED_BLINK_FOREVER);
+                    break;
+
+                default:
+//                PIN_setOutputValue(pinHandle, CONFIG_PIN_RLED, 0);
+//                PIN_setOutputValue(pinHandle, CONFIG_PIN_GLED, 0);
+                    break;
+            }
+        }
+    }
+}
---
+++ BlinkAtFrequencyTx/Debug/easylink/subdir_rules.mk
@@ -0,0 +1,15 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+easylink/%.obj: ../easylink/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: Arm Compiler'
+	"F:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me -O3 --include_path="F:/ScientificInitiationProjects/BlinkAtFrequencyTx" --include_path="F:/ScientificInitiationProjects/BlinkAtFrequencyTx/Debug" --include_path="F:/ti/ccs1110/simplelink_cc13xx_cc26xx_sdk_6_10_00_29/source" --include_path="F:/ti/ccs1110/simplelink_cc13xx_cc26xx_sdk_6_10_00_29/kernel/tirtos7/packages" --include_path="F:/ti/ccs1110/simplelink_cc13xx_cc26xx_sdk_6_10_00_29/source/ti/posix/ccs" --include_path="F:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include" --define=DeviceFamily_CC13X2 -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="easylink/$(basename $(<F)).d_raw" --include_path="F:/ScientificInitiationProjects/BlinkAtFrequencyTx/Debug/syscfg" --obj_directory="easylink" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
